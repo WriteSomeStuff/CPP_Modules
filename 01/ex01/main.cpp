ce9a144e1470118e34015bb741d4ae62b689c293
Cheyenne Schabracq <cschabra@f0r3s13.codam.nl>
2024-01-23 14:33:27 +0100

    Add program to gitignore and fix small mistake in output comment from wrong input, 1 to 0

--- 01/ex01/main.cpp
+++ 01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cschabra <cschabra@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/11/20 16:09:05 by cschabra      #+#    #+#                 */
-/*   Updated: 2023/12/28 14:46:19 by cschabra      ########   odam.nl         */
+/*   Updated: 2024/01/23 14:23:07 by cschabra      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int	main(void)
 	std::stringstream ss(input);
 	if (!(ss >> amount) || amount < 1 || amount > 50000)
 	{
-		std::cout << "More than one and no higher than 50000!\n";
+		std::cout << "More than 0 and no higher than 50000!\n";
 		return (EXIT_FAILURE);
 	}
 	std::cout << "Enter the name of the horde: ";
