14f110b7d21211c953a06e90565ada5da4a61ae3
Cheyenne Schabracq <cschabra@f0r3s13.codam.nl>
2024-05-10 12:54:54 +0200

    Add empty functions

--- 05/ex00/Bureaucrat.cpp
+++ 05/ex00/Bureaucrat.cpp
@@ -6,9 +6,49 @@
 /*   By: cschabra <cschabra@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/05/09 14:11:46 by cschabra      #+#    #+#                 */
-/*   Updated: 2024/05/09 14:11:56 by cschabra      ########   odam.nl         */
+/*   Updated: 2024/05/10 12:47:11 by cschabra      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
+void	Bureaucrat::setGrade(int grade)
+{
+	_grade = grade;
+}
+
+int	Bureaucrat::getGrade()
+{
+	return (_grade);
+}
+
+std::string	Bureaucrat::getName()
+{
+	return (_name);
+}
+
+Bureaucrat::Bureaucrat(const std::string name) : _name(name)
+{
+	
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& bureaucrat)
+{
+	
+}
+
+Bureaucrat&	Bureaucrat::operator=(const Bureaucrat& bureaucrat)
+{
+
+}
+
+// Is it cheap or expensive to copy a class from Bureaucrat? Should I pass by reference or value here?
+std::ostream&	operator<<(std::ostream& co, const Bureaucrat& bureaucrat)
+{
+	
+}
+
+Bureaucrat::~Bureaucrat()
+{
+
+}
--- 05/ex00/Bureaucrat.hpp
+++ 05/ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: cschabra <cschabra@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/05/09 14:10:53 by cschabra      #+#    #+#                 */
-/*   Updated: 2024/05/09 14:11:28 by cschabra      ########   odam.nl         */
+/*   Updated: 2024/05/10 12:39:01 by cschabra      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,24 @@
 class Bureaucrat
 {
 	protected:
-	std::string	_type;
+	const std::string	_name;
+	int					_grade;
 
 	public:
 
-	Bureaucrat();
+	void		setGrade(int grade);
+	int			getGrade();
+	std::string	getName();
+	void		incrementGrade();
+	void		decrementGrade();
+
+	Bureaucrat(const std::string name);
 	Bureaucrat(const Bureaucrat& Bureaucrat);
 	Bureaucrat&	operator=(const Bureaucrat& Bureaucrat);
 	virtual	~Bureaucrat();
+
 };
+
+std::ostream&	operator<<(std::ostream& co, const Bureaucrat& bureaucrat);
 
 #endif
