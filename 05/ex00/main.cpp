a8a907d73e96d79ccacef9d046b69f058ac3d62e
Cheyenne Schabracq <cschabra@f1r1s12.codam.nl>
2024-07-02 13:12:17 +0200

    Add test cases for all constructors

--- 05/ex00/main.cpp
+++ 05/ex00/main.cpp
@@ -5,15 +5,17 @@ int	main()
 	Bureaucrat	a("Lillia", 1);
 	Bureaucrat	b("Smolder", 150);
 	Bureaucrat	c("Gnar", 66);
+	std::cout << "----------------------------------------------------\n";
 
 	std::cout << a;
 	std::cout << b;
 	std::cout << c;
+	std::cout << "----------------------------------------------------\n";
 
 	Bureaucrat	d("Warwick", 0);
 	Bureaucrat	e("Nami", -1);
 	Bureaucrat	f("Ahri", 157);
-
+	std::cout << "----------------------------------------------------\n";
 	a.incrementGrade();
 	std::cout << a;
 	a.decrementGrade();
@@ -26,4 +28,14 @@ int	main()
 	std::cout << c;
 	c.decrementGrade();
 	std::cout << c;
+	std::cout << "----------------------------------------------------\n";
+
+	// Bureaucrat	g(a);
+	// Bureaucrat	h;
+
+	// std::cout << h;
+	// std::cout << g;
+	// g = a;
+	// std::cout << g;
+	// std::cout << "----------------------------------------------------\n";
 }
