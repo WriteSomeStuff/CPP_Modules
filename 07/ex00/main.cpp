e1e31ecf409c8009e9574c5d0acd0d45b648b540
Cheyenne Schabracq <cschabra@f1r2s3.codam.nl>
2024-08-30 16:10:03 +0200

    Add testcase

--- 07/ex00/main.cpp
+++ 07/ex00/main.cpp
@@ -1,8 +1,36 @@
 #include "whatever.hpp"
 #include <iostream>
 
+// class Test
+// {
+// 	private:
+// 	int	_i;
+
+// 	public:
+// 	Test() : _i(0)
+// 	{
+// 	}
+// 	Test(int i): _i(i)
+// 	{
+// 	}
+// 	int getInt() const
+// 	{
+// 		return (_i);
+// 	}
+// };
+// std::ostream& operator<<(std::ostream&o, const Test& t)
+// {
+// 	o << t.getInt();
+// 	return (o);
+// }
+
 int main( void )
 {
+	// Test t1(1), t2(6);
+
+
+	// ::swap( t1, t2 );
+	// std::cout << "t1 = " << t1 << ", t2 = " << t2 << std::endl;
 	int a = 2;
 	int b = 3;
 	::swap( a, b );
