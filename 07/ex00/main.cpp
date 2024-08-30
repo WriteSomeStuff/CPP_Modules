d5a9a33b3032b2979cb7b595143539388b91837f
Cheyenne Schabracq <cschabra@f1r2s3.codam.nl>
2024-08-30 16:25:17 +0200

    Add testcase

--- 07/ex00/main.cpp
+++ 07/ex00/main.cpp
@@ -1,36 +1,35 @@
 #include "whatever.hpp"
 #include <iostream>
 
-// class Test
-// {
-// 	private:
-// 	int	_i;
+class Test
+{
+	private:
+	int	_i;
 
-// 	public:
-// 	Test() : _i(0)
-// 	{
-// 	}
-// 	Test(int i): _i(i)
-// 	{
-// 	}
-// 	int getInt() const
-// 	{
-// 		return (_i);
-// 	}
-// };
-// std::ostream& operator<<(std::ostream&o, const Test& t)
-// {
-// 	o << t.getInt();
-// 	return (o);
-// }
+	public:
+	Test() : _i(0)
+	{
+	}
+	Test(int i): _i(i)
+	{
+	}
+	int getInt() const
+	{
+		return (_i);
+	}
+};
+std::ostream& operator<<(std::ostream&o, const Test& t)
+{
+	o << t.getInt();
+	return (o);
+}
 
 int main( void )
 {
-	// Test t1(1), t2(6);
+	Test t1(1), t2(6);
 
-
-	// ::swap( t1, t2 );
-	// std::cout << "t1 = " << t1 << ", t2 = " << t2 << std::endl;
+	::swap( t1, t2 );
+	std::cout << "t1 = " << t1 << ", t2 = " << t2 << std::endl;
 	int a = 2;
 	int b = 3;
 	::swap( a, b );
--- 07/ex01/main.cpp
+++ 07/ex01/main.cpp
@@ -1,17 +1,44 @@
 #include "iter.hpp"
 #include <iostream>
+#include "iter.hpp"
+#include <iostream>
+
+class Test
+{
+	private:
+	int	_i;
+
+	public:
+	Test() : _i(0)
+	{
+	}
+	Test(int i): _i(i)
+	{
+	}
+	int getInt() const
+	{
+		return (_i);
+	}
+};
+std::ostream& operator<<(std::ostream&o, const Test& t)
+{
+	o << t.getInt();
+	return (o);
+}
 
 template<typename T>
 void increase(T n)
 {
-	std::cout << typeid(n).name() << ": " << n + 1 << '\n';
+	std::cout << typeid(n).name() << ": " << n << '\n';
 }
 
 int	main(void)
 {
 	int		array1[] = {1, 2, 3, 4, 5};
 	float	array2[] = {1.1f, 2.2f, 3.3f, 4.4f, 5.5f};
+	Test	test[5] = {4, 5, 6, 7, 8};
 
 	iter(array1, sizeof(array1) / sizeof(int), &increase);
 	iter(array2, sizeof(array2) / sizeof(float), &increase);
+	iter(test, sizeof(array2) / sizeof(Test), &increase);
 }
