55070d60cb8a86f18f1548e65634f38e39121e14
Cheyenne Schabracq <cschabra@f0r4s9.codam.nl>
2024-10-16 18:18:22 +0200

    Add input file open function

--- 09/ex00/main.cpp
+++ 09/ex00/main.cpp
@@ -1,5 +1,26 @@
 #include "BitcoinExchange.hpp"
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+void	readInputFile(char* inputFile)
+{
+	std::ifstream	srcFile;
+	std::string		str;
+
+	srcFile.open(inputFile, std::ios::in);
+	if (srcFile.fail())
+	{
+		std::cerr << "Error: could not open file.\n";
+		std::exit(1);
+	}
+	while (std::getline(srcFile, str))
+	{
+		std::cout << str << '\n';
+		// use every line to try feed it into database checker
+	}
+	srcFile.close();
+}
 
 int	main(int argc, char** argv)
 {
@@ -8,8 +29,8 @@ int	main(int argc, char** argv)
 		std::cerr << "Please try ./btc input.txt\n";
 		return (1);
 	}
+	readInputFile(argv[1]);
 	// parse database to access and find closest date (lower if not exact)
-	// open input file, grab every line to try feed it into database checker
 	// if worked, write results, if not throw right error. continue to next line.
 
 }
